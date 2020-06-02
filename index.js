3bd2785db249a61c49256f5ae56b8081a3d4f93b
marta-canizares <marta.s.canizares@gmail.com>
2020-06-02 13:58:04 +0200

    ok

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
+
 const restPhoneApi = axios.create({
-    baseURL: 'https://stark-depths-07561.herokuapp.com/api/persons'
+    baseURL: 'https://stark-depths-07561.herokuapp.com'
 })
+
 const getAllPhone = () => {
     restPhoneApi
-        .get()
+        .get('api/persons')
         .then(responseFromApi => {
             console.log(responseFromApi.data);
             document.write(JSON.stringify(responseFromApi.data))
         })
         .catch(err => console.log('error', err));
 }
-document.getElementById('theButton').onclick = getAllPhone;
 
-// crear un servidor para hacer pruebas
+const hacerUnPost = () => {
+    const credentials = {
+        user: document.getElementById('user').value,
+        password: document.getElementById('password').value,
+    }
+    restPhoneApi
+        .post('login', credentials)
+        .then(res => console.log('login successful'))
+        .catch(e => console.log(e))
+}
+
+const hacerUnLogOut = () => {
+    restPhoneApi
+        .get('logout')
+        .then(responseFromApi => {
+            console.log(responseFromApi.data);
+        })
+        .catch(err => console.log('error', err));
+}
+
+
+// el evento onclick
+document.getElementById('theButton').onclick = getAllPhone;
+document.getElementById('login').onclick = hacerUnPost;
+document.getElementById('logout').onclick = hacerUnLogOut;
